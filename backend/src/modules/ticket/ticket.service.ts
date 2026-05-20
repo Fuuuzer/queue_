d09b60d63225d24e0dd1b9b5b1dbaab594e8fa44
Joao Fuzer <fuuzer_@hotmail.com>
2026-05-19 22:58:20 -0300

    add historyTicket

--- backend/src/modules/ticket/ticket.service.ts
+++ backend/src/modules/ticket/ticket.service.ts
@@ -7,7 +7,8 @@ const transitions = {
   CLOSED: [],
   RESOLVED : ['OPEN']
 }as Record<string, string[]>
-type Status = keyof typeof transitions;
+
+type Status = keyof typeof transitions; //transforma status = OPEN | IN_PROGRESS | CLOSED | RESOLVED 
 
 export const createTicket = async (data: {
   title: string;
@@ -59,14 +60,28 @@ export const deleteUnique = async ({ticketNumber}:{ticketNumber:number}) => {
 }
 
 export const updateTicketStatus = async ({ticketNumber, newStatus}: {ticketNumber: number, newStatus:Status}) => {
+
   const ticket = await prisma.ticket.findUnique({ where: {ticketNumber} });
   if (!ticket) {
     throw new AppError('Ticket nao encontrado', 404)
   }
-  const statusAtual = ticket.status as keyof typeof transitions; //typeof transforma a variavel em tipo
+  const statusAtual = ticket.status as keyof typeof transitions; //garante que o valor recebido eh valido
   if (!transitions[statusAtual].includes(newStatus)) {
     throw new AppError('Transição de status inválida', 400)
   }
+  const oldStatus = statusAtual;
+
+   await prisma.ticketHistory.create({
+    data: {
+      ticketId : ticket.id,
+      from: oldStatus,
+      to : newStatus
+    }
+  })
+
+  // console.log("Ticket vindo do banco:", ticket);
+  // console.log("Status atual:", ticket.status);
+  // console.log("Novo status:", newStatus);
   return await prisma.ticket.update({
     where: {
       ticketNumber
