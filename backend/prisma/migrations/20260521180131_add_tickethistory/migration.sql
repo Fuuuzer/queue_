82cf3e86e03f4e77467bbe188ff22af9efda9cbe
Joao Fuzer <fuuzer_@hotmail.com>
2026-05-21 15:02:19 -0300

    add migration

---
+++ backend/prisma/migrations/20260521180131_add_tickethistory/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `TicketHistory` (
+    `id` VARCHAR(191) NOT NULL,
+    `ticketId` VARCHAR(191) NOT NULL,
+    `from` VARCHAR(191) NOT NULL,
+    `to` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `TicketHistory` ADD CONSTRAINT `TicketHistory_ticketId_fkey` FOREIGN KEY (`ticketId`) REFERENCES `tickets`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
