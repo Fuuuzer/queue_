f58612b246b453566ca361f4021877ca224ba162
Joao Fuzer <fuuzer_@hotmail.com>
2026-05-25 20:25:54 -0300

    mergin

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,5 +8,6 @@ const app = express();
 app.use(express.json());
 app.use('/tickets', ticketRoutes)
 app.use('/users', userRoutes)
+app.use('/auth', userRoutes)
 app.use(errorHandler);
 export default app;
