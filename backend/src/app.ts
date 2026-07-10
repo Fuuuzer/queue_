cfd5a28fd5558edb3e677ec183ab4b38e1f706f4
Joao Fuzer <fuuzer_@hotmail.com>
2026-07-10 16:12:43 -0300

    add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { ticketRoutes } from "./modules/ticket/ticket.routes";
 import { errorHandler } from "./middlewares/errorHandler";
 import { userRoutes } from "./modules/user/user.routes";
 import { authRoutes } from "./modules/auth/auth.routes";
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use('/tickets', ticketRoutes)
