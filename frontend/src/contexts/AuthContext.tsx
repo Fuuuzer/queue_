908bc279ff1406a8c8b7b257bb19856bbb01d8bb
Joao Fuzer <fuuzer_@hotmail.com>
2026-07-13 17:15:05 -0300

    started authContext

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const AuthContext = () => {
-  return (
-    <div>AuthContext</div>
-  )
-}
+  interface AuthContextData {
+    token: string | null;
+    login: (token: string) => void;
+    logout: () => void;
+    isAuthenticated: boolean;
+  } // "prateleira, define os itens que ficarao disponiveis para o contexto de autenticação"
+
+const AuthContext = React.createContext<AuthContextData | undefined>(undefined); // "cria o contexto de autenticação, que será usado para fornecer e consumir os dados de autenticação em toda a aplicação"
 
 export default AuthContext
