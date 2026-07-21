135b0a25741fc46cb1b715dd8a3bbacfd1fd429f
Joao Fuzer <fuuzer_@hotmail.com>
2026-07-21 11:31:46 -0300

    continuando o auth.ts

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,13 +1,17 @@
 import instance from "./api";
 
-//nomes de interface
+//nomes de interface começam com letra maiuscula
 interface LoginCredentials {
   email: string;
   password: string;
 }
 
-export const AuthLogin = async (credentials: LoginCredentials) => { // o tipo aqui ele puxa exatamente o que foi colocado dentro da interface
-  const user = await instance.post('/auth/login', credentials)
 
-  return user
+
+export const AuthLogin = async (credentials: LoginCredentials) => { // o tipo aqui ele puxa exatamente o que foi colocado dentro da interface
+
+  const response = await instance.post('/auth/login', {email: 'joaofuzer@gmail.com', password:'12345'});
+  const {data: loginData, message: loginMessage} = response.data;
+
+  console.log(loginData, loginMessage)
 }
