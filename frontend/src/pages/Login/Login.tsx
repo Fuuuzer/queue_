b23dfe04bd2829884cfd1bc39ec46a822261bc01
Joao Fuzer <fuuzer_@hotmail.com>
2026-07-22 15:30:21 -0300

    started login page

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,13 +1,37 @@
 import React from 'react'
 import { AuthLogin } from '../../api/auth'
+import { useAuth } from '../../contexts/AuthContext';
 
 const Login = () => {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
-  const [error, setError] = React.useState<string | null>()
+  const [error, setError] = React.useState<string | null>();
+  const {login} = useAuth();
+  
+  function handleSubmit(e: React.ChangeEvent<HTMLFormElement>) {
+    e.preventDefault()
+  }
+
 
   return (
-    <div>Login</div>
+    <form action="">
+      <label htmlFor="email">Email</label>
+      <input
+       id='email'
+       type="text"
+       value={email}
+       onChange={(e) => setEmail(e.target.value)}
+       />
+       {email}
+      <label htmlFor="password">Senha</label>
+      {password}
+      <input 
+       id='password' 
+       type="password" 
+       value={password}
+       onChange={(e) => setPassword(e.target.value)}/>
+       <button type="submit" onClick={handleSubmit}></button>
+    </form>
   )
 }
 
