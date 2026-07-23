b5aed961d812fb246c51edf0b4926f57b7156362
Joao Fuzer <fuuzer_@hotmail.com>
2026-07-23 09:56:42 -0300

    fazendo form de login

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,36 +1,38 @@
 import React from 'react'
 import { useAuth } from '../../contexts/AuthContext';
+import { AuthLogin } from '../../api/auth';
 
 const Login = () => {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
   const [error, setError] = React.useState<string | null>();
+  const { login } = useAuth();
 
-  
+
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
-    
+    const token = async () => {
+      await AuthLogin({ email, password })
+      await console.log(token)
+    }
   }
-
 
   return (
     <form action="" onSubmit={handleSubmit}>
       <label htmlFor="email">Email</label>
       <input
-       id='email'
-       type="text"
-       value={email}
-       onChange={(e) => setEmail(e.target.value)}
-       />
-       {email}
+        id='email'
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
       <label htmlFor="password">Senha</label>
-      {password}
-      <input 
-       id='password' 
-       type="password" 
-       value={password}
-       onChange={(e) => setPassword(e.target.value)}/>
-       <button type="submit" ></button>
+      <input
+        id='password'
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Enviar</button>
     </form>
   )
 }
