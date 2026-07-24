d624eb8c0f679e678c44c2e2f3d1dbe9301a752c
Joao Fuzer <fuuzer_@hotmail.com>
2026-07-24 14:55:53 -0300

    logica de login

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,24 +1,33 @@
 import React from 'react'
 import { useAuth } from '../../contexts/AuthContext';
 import { AuthLogin } from '../../api/auth';
+import { isAxiosError } from 'axios';
 
 const Login = () => {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
   const [error, setError] = React.useState<string | null>();
+  const [success, setSuccess] = React.useState<string | null>();
   const { login } = useAuth();
   const [isRunning, setIsRunning] = React.useState<boolean>(false)
 
 
    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    setIsRunning(true)
-    e.preventDefault()
+    setIsRunning(true);
+    setError(null);
+    setSuccess(null);
+    e.preventDefault();
     try {
       const response = await  AuthLogin({email, password});
       login(response.token);
+      setSuccess('Usuário logado com sucesso!')
+
     } catch (err) {
-      setError('o que coloco aq ???')
-      console.error(err)
+      if (isAxiosError(err)) {
+        setError(err.response?.data.message);
+      } else {
+        setError('Houve um erro ao fazer o login')
+      }
     } finally {
       setIsRunning(false)
     } 
@@ -26,10 +35,12 @@ const Login = () => {
 
   return (
     <form action="" onSubmit={handleSubmit}>
+      { error && <p style={error ? {color: 'red',} : {color: 'green'}}>{error || success}</p> || success && <p style={error ? {color: 'red',} : {color: 'green'}}>{error || success}</p>}
+      
       <label htmlFor="email">Email</label>
       <input
         id='email'
-        type="text"
+        type="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
