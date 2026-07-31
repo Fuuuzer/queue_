4d04de4de536427c395ce7feec076ad17ce154d8
Joao Fuzer <fuuzer_@hotmail.com>
2026-07-31 12:07:23 -0300

    puxando os tickets bonitnnho agora

--- frontend/src/pages/Tickets/Tickets.tsx
+++ frontend/src/pages/Tickets/Tickets.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import instance from '../../api/api'
+import { isAxiosError } from 'axios'
+
+
+interface TicketData {
+  title: string,
+  description: string,
+  ticketNumber: number,
+  status: 'OPEN' | 'IN_PROGRESS' | 'RESOLVED' | 'CLOSED',
+  priority: 'LOW' | 'MEDIUM' | 'CRITICAL'
+}
 
 const Tickets = () => {
-  const [tickets, setTickets] = React.useState<[]>([])
-  const [error, setError] = React.useState<string>('');
-  
+  const [tickets, setTickets] = React.useState<TicketData[]>([])
+  const [error, setError] = React.useState<string | null>('');
 
   React.useEffect(() => {
+    setError(null)
     const fetchData = async () => {
       try {
         const token = localStorage.getItem('token')
@@ -15,9 +25,11 @@ const Tickets = () => {
             'Authorization': `Bearer ${token}`
           }
         });
-        console.log(response.data)
-        setTickets(response.data)
+        const responseData = response.data.data
+        console.log(responseData)
+        setTickets(responseData)
       } catch (err) {
+        if(isAxiosError(err))
         setError(err.message || 'Something went wrong');
       }
     }
@@ -25,13 +37,17 @@ const Tickets = () => {
   }, [])
   return (
     <>
-    
-    {error}
+    {/* {tickets} */}
     <h1>Tickets</h1>
-    {tickets.map(item => (
-      <li key={item.title}>{item.name}</li>
-      
-    ))}
+    <p>{error}</p>
+    <>{tickets.map(item => (
+       <div key={item.ticketNumber}>
+        <h1>{item.title}</h1>
+        <p>{item.description}</p>
+        <p>{item.status}</p>
+       </div>
+    ))}</>
+ 
     
     </>
   )
