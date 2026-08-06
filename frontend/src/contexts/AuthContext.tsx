73df4130470beb964088e2b074e81b2d57fd667e
Joao Fuzer <fuuzer_@hotmail.com>
2026-08-06 13:41:52 -0300

    tokenValidate

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { isTokenExpired } from '../utils/token';
 
 interface AuthContextData {
   token: string | null;
@@ -9,9 +10,16 @@ interface AuthContextData {
 
 const AuthContext = React.createContext<AuthContextData | undefined>(undefined); // cria o contexto de autenticação, que será usado para fornecer e consumir os dados de autenticação em toda a aplicação
 
-export function AuthProvider({ children }: { children: React.ReactNode }) {
-  const [token, setToken] = React.useState<string | null>( // utilizei generics, para definir o tipo do estado, que pode ser string ou null.
-    localStorage.getItem("token")
+export function AuthProvider({ children }: { children: React.ReactNode }) { // utilizei generics, para definir o tipo do estado, que pode ser string ou null.
+  const [token, setToken] = React.useState<string | null>(() =>{
+    const tokenStored = localStorage.getItem("token");
+    if(isTokenExpired(tokenStored)) {
+      console.log(tokenStored) 
+      localStorage.removeItem('token')
+      return null
+    }
+    return tokenStored
+    } 
   );
 
   function login(newToken: string) {
