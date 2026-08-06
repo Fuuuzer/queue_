6f30b612550cff64688c69291c84885e9ddc63b4
Joao Fuzer <fuuzer_@hotmail.com>
2026-08-06 16:11:01 -0300

    interceptor axios

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -4,4 +4,17 @@ const instance = axios.create({
   baseURL: 'http://localhost:3000'
 });
 
+
+instance.interceptors.response.use(
+  function (response) {
+    return response
+  },
+  function (error) {
+    if(error.response?.status === 401){
+      window.dispatchEvent(new CustomEvent('auth:expired'))
+    }
+    return Promise.reject(error)
+  }
+)
+
 export default instance
