721eb800260869f0e7ee8725c3207ec1da1ff1c0
Joao Fuzer <fuuzer_@hotmail.com>
2026-08-18 23:31:42 -0300

    create ticket done, doing another things

--- frontend/src/pages/CreateTicket/CreateTicket.tsx
+++ frontend/src/pages/CreateTicket/CreateTicket.tsx
@@ -1,23 +1,44 @@
 import React from 'react'
 import { createTicket } from '../../api/tickets';
+import { isAxiosError } from 'axios';
+
+interface Feedback {
+  type: 'error' | 'success';
+  message: string;
+}
 
 const CreateTicketForm = () => {
-  const [title, setTitle] = React.useState<string>('')
-  const [description, setDescription] = React.useState<string>('')
+  const [title, setTitle] = React.useState<string>('');
+  const [description, setDescription] = React.useState<string>('');
+  const [feedback, setFeedback] = React.useState<Feedback | null>(null)
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
    e.preventDefault();
+   setFeedback(null)
+
+   if(title === '' || description === '') {
+    setFeedback({type: 'error', message: 'É necessário preencher todos os camos para registrar o ticket! /n :| '})
+   }
+   
    try {
     const response = await createTicket({title, description})
-    console.log(response)
-   } catch (err){
-    console.error(err)
-   }
+    const ticket = response.data;
+    setFeedback({type: 'success', message: 'Ticket criado com sucesso!'})
+    console.log(ticket)
+   } catch (err) {
+    if(isAxiosError(err)) {
+      console.error(err.response?.status)
+    } else {
+      console.error(err)
+      setFeedback({type: 'error', message: 'Ocorreu um erro ao registrar o ticket'})
+    }
   }
+}
 
   return (
     <>
     <form onSubmit={handleSubmit}>
+      {feedback && <p style={{color: feedback.type === 'error' ? 'red' : 'green'}} >{feedback.message}</p>}
       <label htmlFor="title">Como podemos lhe ajudar?</label>
 
       <input 
